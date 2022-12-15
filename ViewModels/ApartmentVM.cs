5e382f980ace891668687ddba7b424137e6b5c74
Alexandr Anokhin <82547576+Shalopai-kabanov@users.noreply.github.com>
2022-12-15 20:06:32 +0500

    Add files via upload

---
+++ ViewModels/ApartmentVM.cs
@@ -0,0 +1,17 @@
+﻿using MB.Data;
+using MB.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MB.ViewModels
+{
+    public class ApartmentVM
+    {
+        public Apartment Apartment { get; set; }
+        public IEnumerable<SelectListItem> TDDHotel { get; set; }
+        public IEnumerable<SelectListItem> TDDType_of_apartment { get; set; }
+    }
+}
---
+++ ViewModels/EmployeeVM.cs
@@ -0,0 +1,17 @@
+﻿using MB.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MB.ViewModels
+{
+    public class EmployeeVM
+    {
+        public Employee Employee { get; set; }
+
+        public IEnumerable<SelectListItem> TDDHotel { get; set; }
+        public IEnumerable<SelectListItem> TDDRole { get; set; }
+    }
+}
